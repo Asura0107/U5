f2520d2d018c2fb2472c57306ead722de48620c4
Asura0107 <145717676+Asura0107@users.noreply.github.com>
2024-02-14 21:34:50 +0100

    Delete u5w2d3 directory

--- u5w2d3/src/main/java/u5w2d3/u5w2d3/exception/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package u5w2d3.u5w2d3.exception;
-
-public class BadRequestException extends RuntimeException {
-    public BadRequestException(String message){
-        super(message);
-    }
-}
--- u5w2d3/src/main/java/u5w2d3/u5w2d3/exception/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package u5w2d3.u5w2d3.exception;
-
-public class NotFoundException extends RuntimeException{
-    public NotFoundException(long id){
-        super("L'utente con l'id "+ id + " non è stato trovato");
-    }
-}
