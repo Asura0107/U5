94fd3e56f9d20b5b00b95bfe573dfb8038528411
Asura0107 <145717676+Asura0107@users.noreply.github.com>
2024-02-21 08:58:35 +0100

    Add files via upload

--- W3/u5w3d1/src/main/java/com/example/u5w3d1/security/config.java
+++ W3/u5w3d1/src/main/java/com/example/u5w3d1/security/config.java
@@ -3,6 +3,7 @@ package com.example.u5w3d1.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -13,6 +14,8 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
+// Serve per dichiarare i permessi di accesso sui singoli endpoints
 public class config {
     @Autowired
     private JWTFilter jwtFilter;
